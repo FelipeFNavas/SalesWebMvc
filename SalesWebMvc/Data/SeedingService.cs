052eb1bcf462586bee0fd96244e6f2b917007ea1
Felipe Navas <felipefnavas@gmail.com>
2022-08-22 07:45:39 -0300

    feat: seed

---
+++ SalesWebMvc/Data/SeedingService.cs
@@ -0,0 +1,37 @@
+﻿using SalesWebMvc.Models;
+using System.Linq;
+
+namespace SalesWebMvc.Data
+{
+    public class SeedingService
+    {
+        private SalesWebMvcContext _context;
+        
+        public SeedingService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public void Seed()
+        {
+            if (_context.Department.Any() || _context.Seller.Any() || _context.SalesRecord.Any())
+            {
+                return;
+            }
+
+            Department d1 = new Department(1, "Computers");
+            Department d2 = new Department(2, "Electronics");
+            Department d3 = new Department(3, "Testing");
+
+            Seller s1 = new Seller(1, "Bob", "@gmail", new System.DateTime(1998, 4, 21), 1000, d1);
+
+            SalesRecord sr1 = new SalesRecord(1, new System.DateTime(), 11000, Models.Enums.SalesStatus.Billed, s1);
+
+            _context.Department.AddRange(d1, d2, d3);
+            _context.Seller.AddRange(s1);
+            _context.SalesRecord.AddRange(sr1);
+
+            _context.SaveChanges();
+        }
+    }
+}
--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Models;
 using Microsoft.Extensions.Options;
+using SalesWebMvc.Data;
 
 namespace SalesWebMvc
 {
@@ -41,14 +42,16 @@ namespace SalesWebMvc
                     options.UseMySql(Configuration.GetConnectionString("SalesWebMvcContext"), builder =>
 builder.MigrationsAssembly("SalesWebMvc")));
 
+            services.AddScoped<SeedingService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, SeedingService seedingService)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                seedingService.Seed();
             }
             else
             {
